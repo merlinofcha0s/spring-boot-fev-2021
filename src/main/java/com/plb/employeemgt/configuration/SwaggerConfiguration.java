dca94af92d0ca5f1a6eea3ce89ced9c151563513
Cyril Casaucau <raiden0610@gmail.com>
2021-02-03 15:20:28 +0100

    Configuration swagger

---
+++ src/main/java/com/plb/employeemgt/configuration/SwaggerConfiguration.java
@@ -0,0 +1,33 @@
+package com.plb.employeemgt.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
+
+@Configuration
+public class SwaggerConfiguration {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.plb.employeemgt.web.rest"))
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "My Vinyl API", "Manage my vinyl",
+                "API TOS",
+                "Terms of service",
+                new Contact("John Doe", "www.example.com", "myeaddress@company.com"),
+                "License of API", "API license URL", Collections.emptyList());
+    }
+}
